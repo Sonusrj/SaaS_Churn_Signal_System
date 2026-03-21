73743fda05d8e4efbcac4496f45e6a85c81168ab
Sonu Saroj <sonusaroj02010@gmail.com>
2026-03-21 14:22:39 +0530

    Initial project

---
+++ test_setup.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import lifelines
+import shap
+import seaborn
+import matplotlib
+import psycopg2
+
+print("pandas:", pd.__version__)
+print("numpy:", np.__version__)
+print("scikit-learn:", sklearn.__version__)
+print("lifelines:", lifelines.__version__)
+print("shap:", shap.__version__)
+print("seaborn:", seaborn.__version__)
+print("All packages loaded successfully!")
